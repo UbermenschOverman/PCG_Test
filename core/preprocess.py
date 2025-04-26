42ff68e4575ea32f8f174fb91ffbc75776861787
UbermenschOverman <minhduc276777@gmail.com>
2025-04-26 19:32:04 +0700

    update preprocess.py

--- core/preprocess.py
+++ core/preprocess.py
@@ -1,9 +1,9 @@
 import os
 import librosa
 import numpy as np
-import tensorflow as tf
-import yaml
 import matplotlib.pyplot as plt
+import yaml
+import argparse
 
 # Load config.yaml
 def load_config(config_path='config/config.yaml'):
@@ -14,20 +14,21 @@ def load_config(config_path='config/config.yaml'):
 # Hàm để lưu Mel-spectrogram thành file ảnh PNG
 def save_mel_spectrogram(wav_file, output_dir, filename):
     y, sr = librosa.load(wav_file, sr=None)
-    mel_spec = librosa.feature.melspectrogram(y=y, sr=sr)  # Cập nhật cách gọi hàm melspectrogram
+    mel_spec = librosa.feature.melspectrogram(y=y, sr=sr)
     mel_spec_db = librosa.power_to_db(mel_spec, ref=np.max)
 
-    # Save mel spectrogram as an image
     plt.figure(figsize=(10, 4))
     librosa.display.specshow(mel_spec_db, x_axis='time', y_axis='mel', sr=sr)
     plt.colorbar(format='%+2.0f dB')
-    
+
     output_path = os.path.join(output_dir, filename)
     plt.savefig(output_path, bbox_inches='tight', pad_inches=0)
     plt.close()
 
+    print(f"Saved: {output_path}")
+
 # Hàm tiền xử lý dữ liệu âm thanh
-def preprocess_data(config_path='config/config.yaml'):
+def preprocess_data(config_path):
     config = load_config(config_path)
     paths = config['data']
 
@@ -35,10 +36,6 @@ def preprocess_data(config_path='config/config.yaml'):
     abnormal_dir = paths['abnormal_dir']
     preprocessed_normal_dir = paths['preprocessed_normal_dir']
     preprocessed_abnormal_dir = paths['preprocessed_abnormal_dir']
-
-    # Tạo thư mục đầu ra nếu chưa tồn tại
-    os.makedirs(preprocessed_normal_dir, exist_ok=True)
-    os.makedirs(preprocessed_abnormal_dir, exist_ok=True)
 
     # Tiền xử lý cho thư mục normal
     for wav_file in os.listdir(normal_dir):
@@ -55,4 +52,8 @@ def preprocess_data(config_path='config/config.yaml'):
             save_mel_spectrogram(wav_path, preprocessed_abnormal_dir, output_filename)
 
 if __name__ == "__main__":
-    preprocess_data()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default='config/config.yaml', help='Path to config.yaml')
+    args = parser.parse_args()
+
+    preprocess_data(args.config)
